ae687d77f73ea29e708d8beb403e9ef40d258dd7
sohamxda7 <sensoham135@gmail.com>
2020-07-12 13:13:08 +0000

    Revert "msm: kgsl Rework mutex::owner inside adreno_dispatcher_idle()"
    This reverts commit ce71b557846655ba460d6076f1638bea04e04127.
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- drivers/gpu/msm/adreno_dispatch.c
+++ drivers/gpu/msm/adreno_dispatch.c
@@ -2849,6 +2849,7 @@ int adreno_dispatcher_idle(struct adreno_device *adreno_dev)
 	struct adreno_dispatcher *dispatcher = &adreno_dev->dispatcher;
 	int ret;
 
+	BUG_ON(!mutex_is_locked(&device->mutex));
 	if (!test_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv))
 		return 0;
 
@@ -2857,8 +2858,8 @@ int adreno_dispatcher_idle(struct adreno_device *adreno_dev)
 	 * mutex is held and device is started
 	 */
 	if (mutex_is_locked(&dispatcher->mutex) &&
-		(__mutex_owner(&dispatcher->mutex) == current))
-		return -EDEADLK;
+		dispatcher->mutex.owner == current)
+		BUG_ON(1);
 
 	adreno_get_gpu_halt(adreno_dev);
 
